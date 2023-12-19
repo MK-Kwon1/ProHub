ae0a1263d9f75518b779c921410ec4d780f36f76
MK Kwon <149921021+MK-Kwon1@users.noreply.github.com>
2023-12-20 03:14:05 +1030

    Add files via upload

---
+++ dev.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGODB_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
+} 
